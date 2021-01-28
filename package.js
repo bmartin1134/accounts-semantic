10692acdf6576808df573a95994ff5a25f248320
Brad Martin <bmartin1134@gmail.com>
2021-01-27 22:28:51 -0800

    sf
    eag

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: 'Semantic UI Components for Useraccounts-React',
   version: '1.0.1',
-  name: 'meteoreact:accounts-semantic',
+  name: 'bmartin1134:accounts-semantic',
   git: 'https://github.com/royGil/useraccounts-react.git',
   documentation: null
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.6.1')
+  api.versionsFrom('1.11.1')
 
   api.use('ecmascript')
   api.mainModule('index.js', 'client')
